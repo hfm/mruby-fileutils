7c12c6e6bf423d8a4da90ac15b1777b515261451
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-09-08 17:23:48 +0900

    Define modules

--- src/mrb_fileutils.c
+++ src/mrb_fileutils.c
@@ -12,60 +12,21 @@
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
-typedef struct {
-  char *str;
-  int len;
-} mrb_fileutils_data;
-
-static const struct mrb_data_type mrb_fileutils_data_type = {
-  "mrb_fileutils_data", mrb_free,
-};
-
-static mrb_value mrb_fileutils_init(mrb_state *mrb, mrb_value self)
-{
-  mrb_fileutils_data *data;
-  char *str;
-  int len;
-
-  data = (mrb_fileutils_data *)DATA_PTR(self);
-  if (data) {
-    mrb_free(mrb, data);
-  }
-  DATA_TYPE(self) = &mrb_fileutils_data_type;
-  DATA_PTR(self) = NULL;
-
-  mrb_get_args(mrb, "s", &str, &len);
-  data = (mrb_fileutils_data *)mrb_malloc(mrb, sizeof(mrb_fileutils_data));
-  data->str = str;
-  data->len = len;
-  DATA_PTR(self) = data;
-
-  return self;
-}
-
-static mrb_value mrb_fileutils_hello(mrb_state *mrb, mrb_value self)
-{
-  mrb_fileutils_data *data = DATA_PTR(self);
-
-  return mrb_str_new(mrb, data->str, data->len);
-}
-
-static mrb_value mrb_fileutils_hi(mrb_state *mrb, mrb_value self)
-{
-  return mrb_str_new_cstr(mrb, "hi!!");
-}
-
 void mrb_mruby_fileutils_gem_init(mrb_state *mrb)
 {
   struct RClass *fileutils;
-  fileutils = mrb_define_class(mrb, "FileUtils", mrb->object_class);
-  mrb_define_method(mrb, fileutils, "initialize", mrb_fileutils_init, MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, fileutils, "hello", mrb_fileutils_hello, MRB_ARGS_NONE());
-  mrb_define_class_method(mrb, fileutils, "hi", mrb_fileutils_hi, MRB_ARGS_NONE());
+  struct RClass *fileutils_verbose;
+  struct RClass *fileutils_nowrite;
+  struct RClass *fileutils_dryrun;
+
+  fileutils = mrb_define_module(mrb, "FileUtils");
+  fileutils_verbose = mrb_define_module_under(mrb, fileutils, "Verbose");
+  fileutils_nowrite = mrb_define_module_under(mrb, fileutils, "NoWrite");
+  fileutils_dryrun = mrb_define_module_under(mrb, fileutils, "DryRun");
+
   DONE;
 }
 
 void mrb_mruby_fileutils_gem_final(mrb_state *mrb)
 {
 }
-
--- test/mrb_fileutils.rb
+++ test/mrb_fileutils.rb
@@ -2,16 +2,9 @@
 ## FileUtils Test
 ##
 
-assert("FileUtils#hello") do
-  t = FileUtils.new "hello"
-  assert_equal("hello", t.hello)
-end
-
-assert("FileUtils#bye") do
-  t = FileUtils.new "hello"
-  assert_equal("hello bye", t.bye)
-end
-
-assert("FileUtils.hi") do
-  assert_equal("hi!!", FileUtils.hi)
+assert("FileUtils") do
+  assert_equal Module, FileUtils.class
+  assert_equal Module, FileUtils::Verbose.class
+  assert_equal Module, FileUtils::NoWrite.class
+  assert_equal Module, FileUtils::DryRun.class
 end
